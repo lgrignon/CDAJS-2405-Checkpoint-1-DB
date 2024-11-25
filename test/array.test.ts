d2c811399c3b62aba4928800118113c8b4cd840e
Paul-Lac <plnlacroix@gmail.com>
2024-11-25 11:50:30 +0100

    Part 2. Pop and shift methods added, with their respective tests

--- test/array.test.ts
+++ test/array.test.ts
@@ -7,11 +7,43 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(0);
   });
 
-  it("should had an element in the array", () => {
+  it("should add an element in the array", () => {
     const newArray = new Arr();
     const result = newArray.push("a");
 
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
+  });
+
+  it("should remove and return the last element of the array", () => {
+    const newArray = new Arr();
+    newArray.value = ["a", "b"];
+    const result = newArray.pop();
+
+    expect(newArray.value).toHaveLength(1);
+    expect(result).toEqual("b");
+  });
+
+  it("should return undefined if the array is empty when pop is called", () => {
+    const newArray = new Arr();
+    const result = newArray.pop();
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should remove and return the first element of the array", () => {
+    const newArray = new Arr();
+    newArray.value = ["a", "b"];
+    const result = newArray.shift();
+
+    expect(newArray.value).toHaveLength(1);
+    expect(result).toEqual("a");
+  });
+
+  it("should return undefined if the array is empty when shift is called", () => {
+    const newArray = new Arr();
+    const result = newArray.shift();
+
+    expect(result).toBeUndefined();
   });
 });
