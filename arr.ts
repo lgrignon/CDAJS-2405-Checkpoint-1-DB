e251781b74fd330d39e637701752793124613e29
aglevillayer <levillayer.annegaelle@gmail.com>
2024-11-25 12:58:51 +0100

    2- write pop and shift methods

--- arr.ts
+++ arr.ts
@@ -13,13 +13,28 @@ class Arr {
   }
 
   pop(): string {
-    const removedElement: string = "";
-    return removedElement;
+    const array = new Array(this.value.length - 1);
+    for (let i = 0; i < this.value.length - 1; i++) {
+      array[i] = this.value[i];
+    }
+    const result = this.value[this.value.length - 1];
+
+    this.value = array;
+
+    return result;
   }
 
   shift(): string {
-    const removedElement: string = "";
-    return removedElement;
+    const array = new Array(this.value.length - 1);
+    for (let i = 1; i < this.value.length; i++) {
+      array[i - 1] = this.value[i];
+    }
+
+    const result = this.value[0];
+
+    this.value = array;
+
+    return result;
   }
 }
 
