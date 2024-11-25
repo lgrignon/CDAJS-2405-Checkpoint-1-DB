b087d524c3a7f64c87058e08dd9d068ade4f9c5a
ALEX KANDEL <alex.kandel@wanadoo.fr>
2024-11-25 11:11:25 +0100

    Step2 Tests

--- arr.ts
+++ arr.ts
@@ -2,14 +2,44 @@ class Arr {
   value: string[] = [];
 
   push(el: string): number {
-    const array = new Array(this.value.length + 1);
+      const array = new Array(this.value.length + 1);
 
-    for (let i = 0; i < this.value.length; i++) {
-      array[i] = this.value[i];
-    }
-    array[this.value.length] = el;
-    this.value = array;
-    return this.value.length;
+      for (let i = 0; i < this.value.length; i++) {
+          array[i] = this.value[i];
+      }
+      array[this.value.length] = el;
+      this.value = array;
+      return this.value.length;
+  }
+
+  pop(): string | undefined {
+      if (this.value.length === 0) {
+          return undefined;
+      }
+
+      const array = new Array(this.value.length - 1);
+      const lastElement = this.value[this.value.length - 1];
+
+      for (let i = 0; i < this.value.length - 1; i++) {
+          array[i] = this.value[i];
+      }
+      this.value = array;
+      return lastElement;
+  }
+
+  shift(): string | undefined {
+      if (this.value.length === 0) {
+          return undefined;
+      }
+
+      const array = new Array(this.value.length - 1);
+      const firstElement = this.value[0];
+
+      for (let i = 1; i < this.value.length; i++) {
+          array[i - 1] = this.value[i];
+      }
+      this.value = array;
+      return firstElement;
   }
 }
 
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,17 +1,54 @@
-import Arr from "../arr";
+import Arr from '../arr';
 
-describe("Array", () => {
-  it("should create an array", () => {
-    const newArray = new Arr();
+describe('Array', () => {
+    it('creates an empty array', () => {
+        const newArray = new Arr();
+        expect(newArray.value).toHaveLength(0);
+    });
 
-    expect(newArray.value).toHaveLength(0);
-  });
+    it('adds a character to the array', () => {
+        const newArray = new Arr();
+        const result = newArray.push('Ezio Auditore');
+        expect(newArray.value).toHaveLength(1);
+        expect(result).toEqual(1);
+    });
 
-  it("should had an element in the array", () => {
-    const newArray = new Arr();
-    const result = newArray.push("a");
+    it('removes and returns the last character from the array using pop', () => {
+        const newArray = new Arr();
+        newArray.push('Altair Ibn-LaʼAhad');
+        newArray.push('Ezio Auditore');
+        const result = newArray.pop();
+        expect(result).toBe('Ezio Auditore');
+        expect(newArray.value).toEqual(['Altair Ibn-LaʼAhad']);
+    });
 
-    expect(newArray.value).toHaveLength(1);
-    expect(result).toEqual(1);
-  });
+    it('returns undefined when pop is called on an empty array', () => {
+        const newArray = new Arr();
+        const result = newArray.pop();
+        expect(result).toBeUndefined();
+    });
+
+    it('removes and returns the first location from the array using shift', () => {
+        const newArray = new Arr();
+        newArray.push('Masyaf');
+        newArray.push('Florence');
+        const result = newArray.shift();
+        expect(result).toBe('Masyaf');
+        expect(newArray.value).toEqual(['Florence']);
+    });
+
+    it('returns undefined when shift is called on an empty array', () => {
+        const newArray = new Arr();
+        const result = newArray.shift();
+        expect(result).toBeUndefined();
+    });
+
+    it('adds and removes artifacts in the array', () => {
+        const newArray = new Arr();
+        newArray.push('Apple of Eden');
+        newArray.push('Shroud of Eden');
+        const lastArtifact = newArray.pop();
+        expect(lastArtifact).toBe('Shroud of Eden');
+        expect(newArray.value).toEqual(['Apple of Eden']);
+    });
 });
