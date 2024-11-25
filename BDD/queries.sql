09a30d9b6ae55cfd2872fd59eff24dcb2ebfc58e
AzraeLandres <azraellandres@gmail.com>
2024-11-25 17:54:39 +0100

    added comments

--- BDD/queries.sql
+++ BDD/queries.sql
@@ -1,7 +1,12 @@
+
+--  1. Show all the offers
 SELECT * FROM offer;
 
+--  2. Show all the offers in Paris
 SELECT * FROM offer WHERE city = 'Paris';
 
+--  3. Show all the infos of the applicants who applied to the offer with id 1
 SELECT * FROM applicant WHERE id IN (SELECT applicant_id FROM applications WHERE offer_id = 1);
 
+--  4. Show all the infos of the applicants who applied to an offer from TechCorp
 SELECT * FROM applicant WHERE id IN (SELECT applicant_id FROM applications WHERE offer_id IN (SELECT id FROM offer WHERE company_id = 1));
