a6de9e0220fd75d3c98b098e4dd7cca8dc177533
AzraeLandres <azraellandres@gmail.com>
2024-11-25 17:07:33 +0100

    add pop method

--- arr.ts
+++ arr.ts
@@ -11,6 +11,18 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  pop(el: string): number {
+    const array = new Array(this.value.length - 1);
+
+    for (let i = 0; i < this.value.length - 1; i++) {
+      array[i] = this.value[i];
+    }
+
+    array[this.value.length] = el;
+    this.value = array;
+    return this.value.length;
+  }
 }
 
 export default Arr;
