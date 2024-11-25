b5635c8651f77a0f84a4e8f27f97fda0ad2d30b4
Florence Buchelet <florence.buchelet@gmail.com>
2024-11-25 12:07:28 +0100

    part_two-tdd

--- arr.ts
+++ arr.ts
@@ -2,15 +2,51 @@ class Arr {
   value: string[] = [];
 
   push(el: string): number {
+    // crée un nouveau tableau avec une entrée en plus
     const array = new Array(this.value.length + 1);
 
+    // copie l'ancien tableau dans le nouveau
     for (let i = 0; i < this.value.length; i++) {
       array[i] = this.value[i];
     }
+    // array est le nouveau tableau
+    // this.value est l'ancien tableau
+    // la valeur du dernier élément du tableau (sa longueur) est el
     array[this.value.length] = el;
+
+    // remplace l'ancien tableau par le nouveau
     this.value = array;
+
     return this.value.length;
   }
+
+  // pop()
+  pop(): string | undefined {
+    const array = new Array(this.value.length - 1);
+    for (let i = 0; i < array.length; i++) {
+      array[i] = this.value[i];
+    }
+    // récupère le dernier élément de l'ancien tableau
+    const lastEl = this.value[this.value.length - 1];
+    this.value = array;
+    return lastEl;
+    // = return this.value.pop();
+  }
+
+  // shift()
+  shift(): string | undefined {
+    const array = new Array(this.value.length - 1);
+    for (let i = 0; i < array.length; i++) {
+      array[i] = this.value[i + 1];
+    }
+    // récupère le premier élément de l'ancien tableau
+    const firstEl = this.value[0];
+    this.value = array;
+    return firstEl;
+    // = return this.value.shift();
+  }
 }
+
+
 
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,31 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  // pop()
+  it("should remove and return the last element in the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+
+    const result = newArray.pop();
+
+    expect(result).toHaveLength(1);
+    expect(result).toEqual("b");
+    expect(newArray.value).toHaveLength(1);
+  })
+
+  // shift()
+  it("should remove and return the first element in the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+
+    const result = newArray.shift();
+
+    expect(result).toHaveLength(1);
+    expect(result).toEqual("a");
+    expect(newArray.value).toHaveLength(1);
+  })
 });
+
