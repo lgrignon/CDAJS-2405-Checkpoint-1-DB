f62f4aa4a1347f61a4043d30e48b5f06222657e2
Florian Schaessens <f.schaessens@gmail.com>
2024-11-25 11:58:42 +0100

    PART 2

--- arr.ts
+++ arr.ts
@@ -11,6 +11,38 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  pop(): string | undefined {
+
+    if (this.value.length === 0) 
+      { return undefined }
+
+    const lastElement = this.value[this.value.length - 1];
+    const array = new Array(this.value.length - 1);
+
+    for (let i = 0; i < this.value.length - 1; i++) {
+      array[i] = this.value[i];
+    }
+
+    this.value = array;
+    return lastElement;
+  }
+
+  shift(): string | undefined {
+
+    if (this.value.length === 0) 
+      { return undefined }
+
+    const firstElement = this.value[0];
+    const array = new Array(this.value.length - 1);
+
+    for (let i = 0; i < this.value.length - 1; i++) {
+      array[i] = this.value[i];
+    }
+
+    this.value = array;
+    return firstElement;
+  }
 }
 
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,45 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  it("should remove last element from the array with .pop()", () => {
+    const newArray = new Arr();
+    newArray.push("a")
+    newArray.push("b")
+    const poped = newArray.pop()
+
+    expect(newArray.value).toHaveLength(1)
+    expect(poped).toEqual("b")
+  })
+
+  // pop()
+
+  it("should return undefined when popping from an empty array", () => {
+    const newArray = new Arr();
+    const popped = newArray.pop();
+
+    expect(newArray.value).toHaveLength(0); 
+    expect(popped).toBeUndefined();
+  });
+
+  // shift()
+
+  it("should remove first element from the array with .shift()", () => {
+    const newArray = new Arr();
+    newArray.push("a")
+    newArray.push("b")
+    const shifted = newArray.shift()
+
+    expect(newArray.value).toHaveLength(1)
+    expect(shifted).toEqual("a")
+  })
+
+  it("should return undefined when shift from an empty array", () => {
+    const newArray = new Arr();
+    const popped = newArray.shift();
+
+    expect(newArray.value).toHaveLength(0); 
+    expect(popped).toBeUndefined();
+  });
+
 });
