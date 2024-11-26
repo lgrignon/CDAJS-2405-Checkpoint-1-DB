f2eb16fa8b3958b7e04bbbc0233a2a603a1f6ba5
lgrignon <louis.grignon@conscience-robotics.com>
2024-11-26 14:43:31 +0100

    test queries

--- BDD/test-queries.sql
+++ BDD/test-queries.sql
@@ -1,2 +1,13 @@
 
 select * from job_offer;
+
+select * from job_offer where city = 'Paris';
+
+select c.* from candidate_job_offer_applications app
+join candidate c on c.id = app.id_candidate
+where app.id_job_offer = 3;
+
+select c.* from candidate_job_offer_applications app
+join candidate c on c.id = app.id_candidate
+join job_offer offer on offer.id = app.id_job_offer
+where offer.id_company = 0;
